80bc31ca7cd1f08d126b0aededd00249ab92ad30
Thiago Barbato <thiagocbbt@gmail.com>
2017-03-21 10:17:01 -0300

    Add method interfaces for transformations

--- include/Controller.h
+++ include/Controller.h
@@ -15,19 +15,23 @@ class Controller {
  	Window win;
 
  public:
-  	Controller() {}
- 	bool add_point(const string&, double, double, utils::Color);
- 	bool add_line(const string&, double, double, double, double, utils::Color);
- 	bool add_polygon(const string&, const std::vector<double>&, utils::Color, bool);
- 	
- 	void delete_obj(const string&);
- 	void clear_world();
- 	
- 	void zoom_in(double);
- 	void zoom_out(double);
- 	void pan_x(double);
- 	void pan_y(double);
- 	//pan_z(double);
+	Controller() {}
+	bool add_point(const string&, double, double, utils::Color);
+	bool add_line(const string&, double, double, double, double, utils::Color);
+	bool add_polygon(const string&, const std::vector<double>&, utils::Color, bool);
+	
+	void delete_obj(const string&);
+	void clear_world();
+	
+	void zoom_in(double);
+	void zoom_out(double);
+	void pan_x(double);
+	void pan_y(double);
+	//pan_z(double);
+
+	void move_obj(const string&, double, double);
+	void rotate_obj(const string&, double, double, double);
+	void scale_obj(const string&, double);
 };
 
 #endif /* _CONTROLLER_H_ */
