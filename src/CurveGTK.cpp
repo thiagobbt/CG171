c71e265de2455f8be245717715041443d5f3f206
viniciusmctf <vinimmbb@gmail.com>
2017-04-20 18:42:18 -0300

    Fix drawing of curves to only connect the correct lines

--- src/CurveGTK.cpp
+++ src/CurveGTK.cpp
@@ -6,15 +6,15 @@ CurveGTK::CurveGTK(std::vector<Coordinate>& coords, utils::Color c)
 void CurveGTK::draw(cairo_surface_t& surface, Window& win) {
     std::cout << "Curve::draw" << std::endl;
     if (coords.size() == 0) return;
-    Coordinate a = win.to_viewport(coords[0]);
-    cairo_t *cr = cairo_create(&surface);
-    cairo_set_source_rgb(cr, color.r, color.g, color.b);
-    cairo_move_to(cr, a.get_x(), a.get_y());
 
-    for (size_t i = 1; i < coords.size(); i++) {
-        Coordinate b = win.to_viewport(coords[i]);
+    for (size_t i = 0; i < coords.size() - 1; i += 2) {
+        Coordinate a = win.to_viewport(coords[i]);
+        cairo_t *cr = cairo_create(&surface);
+        cairo_set_source_rgb(cr, color.r, color.g, color.b);
+        cairo_move_to(cr, a.get_x(), a.get_y());
+        Coordinate b = win.to_viewport(coords[i+1]);
         cairo_line_to(cr, b.get_x(), b.get_y());
+        cairo_stroke(cr);
     }
 
-    cairo_stroke(cr);
 }
